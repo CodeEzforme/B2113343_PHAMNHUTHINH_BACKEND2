21c6b3cd3ad3496f10853ccf59455bbf4570b6a2
CodeEzforme <144590228+CodeEzforme@users.noreply.github.com>
2024-01-22 19:53:03 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const contactsRouter = require('./app/routes/contact.route')
-const ApiError = require('./app/api-error.js')
+const ApiError = require('./app/api-error')
 
 const app = express()
 
@@ -14,4 +14,13 @@ app.get('/', (req, res) => {
     res.json({ message: "Welcome to contact book application."})
 })
 
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource Not Found"))
+})
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error"
+    })
+})
 module.exports = app
